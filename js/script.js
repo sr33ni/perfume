36b497dc5fa9ab29793212e864be101743ac2a3d
sreeni <sreenivas.s@zerone-consulting.com>
2026-02-25 18:43:10 +0530

    new commit

--- js/script.js
+++ js/script.js
@@ -89,3 +89,52 @@ if (searchBtn && searchBox && shopNowBtn) {
     shopNowBtn.classList.toggle('hidden');
   });
 }
+
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+/* Main navigation */
+let panelsSection = document.querySelector("#panels"),
+  panelsContainer = document.querySelector("#panels-container"),
+  tween;
+document.querySelectorAll(".anchor").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    console.log("click");
+    e.preventDefault();
+    let targetElem = document.querySelector(e.target.getAttribute("href")),
+      y = targetElem;
+    if (targetElem && panelsContainer.isSameNode(targetElem.parentElement)) {
+      let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+        totalMovement = cont.scrollWidth - innerWidth;
+      y = Math.round(
+        tween.scrollTrigger.start +
+          (targetElem.offsetLeft / totalMovement) * totalScroll
+      );
+    }
+    gsap.to(window, {
+      scrollTo: {
+        y: y,
+        autoKill: false
+      },
+      duration: 1
+    });
+  });
+});
+
+/* Panels */
+const cont = document.querySelector("#panels-container");
+const panels = gsap.utils.toArray("#panels-container .panel");
+
+tween = gsap.to(panels, {
+  x: () => -1 * (cont.scrollWidth - innerWidth),
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#panels-container",
+    pin: true,
+    start: "top top",
+    scrub: 0.2,
+     end: () => "+=" + (cont.scrollWidth - innerWidth) / 4,
+    onUpdate: (self) => {
+     console.log(self.progress)
+    }
+  }
+});
